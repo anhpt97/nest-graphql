2a0bdb1a48ca142523deb4bfd4679982be2ed021
dlchau <dlchau@cmcglobal.vn>
2022-07-22 16:15:39 +0700

    .

--- src/modules/console/console.service.ts
+++ src/modules/console/console.service.ts
@@ -1,19 +1,22 @@
 import bcrypt from 'bcrypt';
 import { Command, Console, createSpinner } from 'nestjs-console';
-import { getRepository } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { UserRole, UserStatus } from '~/common/enums';
 import { User } from '~/entities';
 
 @Console()
 export class ConsoleService {
+  constructor(private dataSource: DataSource) {}
+
   @Command({ command: 'seed' })
   async seed() {
     const spin = createSpinner();
     spin.start('Seeding...\n');
-    const user = await getRepository(User).findOne({
+    const userRepository = this.dataSource.getRepository(User);
+    const user = await userRepository.findOneBy({
       id: '1000000000000000000',
     });
-    await getRepository(User).save({
+    await userRepository.save({
       ...user,
       id: '1000000000000000000',
       username: 'superadmin',
